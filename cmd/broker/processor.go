bc8937e7ca3e6dc3dfb5537c846ef2a40680e8aa
yuzihao0923 <zihao.yu@ucdconnect.ie>
2024-06-02 14:53:49 +0100

    update errors

--- cmd/broker/processor.go
+++ cmd/broker/processor.go
@@ -9,16 +9,15 @@ import (
 
 type Processor struct {
 	Conn net.Conn
-	Tr   *network.Transport
 }
 
 func (p *Processor) brokerProcessMes() (err error) {
-	p.Tr = &network.Transport{
+	tr := &network.Transport{
 		Conn: p.Conn,
 	}
 
 	for {
-		mes, err := p.Tr.ReceiveMessage(p.Tr.Conn)
+		mes, err := tr.ReceiveMessage(tr.Conn)
 		if err != nil {
 			if err.Error() == "EOF" {
 				log.LogMessage("INFO", "Client closed the connection")
@@ -62,35 +61,4 @@ func (p *Processor) removeConsumer(conn net.Conn) {
 			break
 		}
 	}
-}
-
-package main
-
-import (
-	"COMP47250-Team-Software-Project/internal/network"
-	"fmt"
-	"net"
-)
-
-type Processor struct {
-	Conn net.Conn
-}
-
-func (p *Processor) brokerProcessMes() (err error) {
-	tr := &network.Transport{
-		Conn: p.Conn,
-	}
-
-	mes, err := tr.ReceiveMessage(tr.Conn)
-	mes, err := p.Tr.ReceiveMessage(p.Tr.Conn)
-	if err != nil {
-		fmt.Println("Broker can not receive message successfully!!")
-		return
-	}
-
-	fmt.Println("Broker received a message: ", mes.Payload)
-	// fmt.Println("The message detail:")
-	// fmt.Println("ID: ", mes.ID)
-	// fmt.Println("Timestamp: ", mes.Timestamp)
-	// fmt.Println("Type: ", mes.Type)
 }
--- internal/network/network.go
+++ internal/network/network.go
@@ -20,6 +20,7 @@ type Transport struct {
 
 *
 */
+
 // SendMessage serializes the message and writes it to the connection
 func (tr *Transport) SendMessage(conn net.Conn, mes message.Message) error {
 	serializer := &serializer.JSONSerializer{}
@@ -35,6 +36,7 @@ func (tr *Transport) SendMessage(conn net.Conn, mes message.Message) error {
 		return err
 	}
 	return nil
+
 }
 
 /*
@@ -46,80 +48,18 @@ func (tr *Transport) SendMessage(conn net.Conn, mes message.Message) error {
 
 *
 */
+
 // ReceiveMessage reads from the connection and deserializes the message
+
 func (tr *Transport) ReceiveMessage(conn net.Conn) (message.Message, error) {
 	reader := bufio.NewReader(conn)
 	Data, err := reader.ReadBytes('\n')
 	if err != nil {
+
 		if err.Error() == "EOF" {
 			return message.Message{}, err
 		}
-		fmt.Println("Error receiving message:", err)
-		return message.Message{}, err
-	}
 
-	deserializer := &serializer.JSONSerializer{}
-	var mes message.Message
-
-	// Use the pointer of mes!!!!!
-	err = deserializer.Deserialize(Data, &mes)
-	if err != nil {
-		fmt.Println("Error deserialzing message:", err)
-		return message.Message{}, err
-	}
-	return mes, err
-}
-
-package network
-
-import (
-	"COMP47250-Team-Software-Project/internal/message"
-	"COMP47250-Team-Software-Project/pkg/serializer"
-	"bufio"
-	"fmt"
-	"net"
-)
-
-type Transport struct {
-	Conn net.Conn
-}
-
-/*
-*
-
-	1.serialize message
-	2.write to conn
-
-*
-*/
-func (tr *Transport) SendMessage(conn net.Conn, mes message.Message) error {
-	serializer := &serializer.JSONSerializer{}
-	Data, err := serializer.Serialize(mes)
-	if err != nil {
-		fmt.Println("Error serializing message:", err)
-		return err
-	}
-
-	_, err = conn.Write(append(Data, '\n'))
-	if err != nil {
-		fmt.Println("Error sending message:", err)
-	}
-	return err
-}
-
-/*
-*
-
-	1.create a reader to read from conn
-	2.read conn
-	3.deserialize
-
-*
-*/
-func (tr *Transport) ReceiveMessage(conn net.Conn) (message.Message, error) {
-	reader := bufio.NewReader(conn)
-	Data, err := reader.ReadBytes('\n')
-	if err != nil {
 		fmt.Println("Error receiving message:", err)
 		return message.Message{}, err
 	}
