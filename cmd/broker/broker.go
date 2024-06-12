c1d724f8d80edd57bd229789fd470178bf4a0e0c
JettZheng <jett.z@outlook.com>
2024-06-12 11:47:44 +0100

    Update: log mechanism, internal/redis, broker, consumer, producer Add: APIs Delete: internal/model, internal/network

--- cmd/broker/broker.go
+++ cmd/broker/broker.go
@@ -1,61 +1,22 @@
 package broker
 
 import (
+	"COMP47250-Team-Software-Project/internal/api"
 	"COMP47250-Team-Software-Project/internal/log"
-	"COMP47250-Team-Software-Project/internal/redis"
 	"fmt"
-	"net"
-	"sync"
+	"net/http"
 )
-
-var consumers []net.Conn
-var consumersMutex sync.Mutex
-
-func goprocess(conn net.Conn) {
-	defer conn.Close()
-
-	processor := Processor{
-		Conn: conn,
-	}
-
-	// processor.handleConnection()
-
-	err := processor.brokerProcessMes()
-	if err != nil {
-		log.LogMessage("ERROR", fmt.Sprintf("Processor error: %v", err))
-	}
-
-}
 
 func StartBroker() {
 	log.LogMessage("INFO", "Starting broker...")
 
-	// Init redis client (Rdb)
-	redis.Initialize("localhost:6379", "", 0)
+	http.HandleFunc("/produce", api.HandleProduce)
+	http.HandleFunc("/register", api.HandleRegister)
+	http.HandleFunc("/consume", api.HandleConsume)
 
-	// Listen on port 8889
 	log.LogMessage("INFO", "Broker listen on port 8889")
-	listen, err := net.Listen("tcp", "0.0.0.0:8889")
+	err := http.ListenAndServe(":8889", nil)
 	if err != nil {
 		log.LogMessage("ERROR", fmt.Sprintf("Broker listen error: %v", err))
-		return
-	}
-	defer listen.Close()
-
-	for {
-		conn, err := listen.Accept()
-		if err != nil {
-			log.LogMessage("ERROR", fmt.Sprintf("Broker listen.Accept() error: %v", err))
-			continue
-		}
-
-		// Register the new consumer
-		consumersMutex.Lock()
-		consumers = append(consumers, conn)
-		consumersMutex.Unlock()
-
-		// Start a goroutine to keep the communication between broker and the client
-		go goprocess(conn)
-
 	}
 }
--- cmd/consumer/consumer.go
+++ cmd/consumer/consumer.go
@@ -2,52 +2,31 @@ package consumer
 
 import (
 	"COMP47250-Team-Software-Project/internal/log"
-	"COMP47250-Team-Software-Project/internal/message"
-	"COMP47250-Team-Software-Project/internal/network"
+	"COMP47250-Team-Software-Project/internal/api"
 	"fmt"
-	"net"
+	"time"
 )
 
 func StartConsumer() {
+	log.LogMessage("INFO", "Starting consumer...")
 
-	log.LogMessage("INFO", "Starting conumer...")
-
-	conn, err := net.Dial("tcp", "localhost:8889")
+	err := api.RegisterConsumer("mygroup")
 	if err != nil {
-		log.LogMessage("ERROR", fmt.Sprintf("Consumer has error connecting to broker: %v", err))
+		log.LogMessage("ERROR", fmt.Sprintf("Consumer has error registering: %v", err))
 		return
-	}
-	defer conn.Close()
-
-	log.LogMessage("INFO", "Consumer has connected to the broker")
-
-	// Currently, do not consider re-connected situation!!!    Every new consumer connection will be treated as first time connection
-
-	mes := message.Message{
-		Type:    "registration",
-		Payload: []byte("mygroup"), // If the Type is registration, Playload will be the consumer group name
-	}
-
-	tr := &network.Transport{
-		Conn: conn,
-	}
-
-	error := tr.SendMessage(mes)
-	if error != nil {
-		log.LogMessage("ERROR", fmt.Sprintf("Consumer has error sending registration message: %v", error))
 	}
 
 	for {
-		mes, err := tr.ReceiveMessage()
+		messages, err := api.ConsumeMessages("myconsumer")
 		if err != nil {
-			if err.Error() == "EOF" {
-				log.LogMessage("INFO", "Broker closed the connection")
-				return
-			}
 			log.LogMessage("ERROR", fmt.Sprintf("Consumer has error receiving message: %v", err))
 			return
 		}
 
-		log.LogMessage("INFO", "Consumer received message: "+string(mes.Payload))
+		for _, mes := range messages {
+			log.LogMessage("INFO", "Consumer received message: "+string(mes.Payload))
+		}
+
+		time.Sleep(time.Second * 2)
 	}
 }
