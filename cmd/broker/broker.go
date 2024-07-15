16d79d8a522ae878ae3ecf3ea4a6b3ba666367cb
Jett <jett.z@outlook.com>
2024-07-15 11:47:32 +0100

    Update: configure path and files

--- cmd/broker/broker.go
+++ cmd/broker/broker.go
@@ -200,7 +200,7 @@ func (b *Broker) sendHeartbeat(proxyURL string) {
 
 func main() {
 	fmt.Println("Starting Broker...")
-	configPath := "/home/yuzihao0923/COMP47250-Team-Software-Project/configs/configloader/brokers.yaml"
+	configPath := "../../configs/configloader/brokers.yaml"
 	// check config file exists
 	if _, err := os.Stat(configPath); os.IsNotExist(err) {
 		log.LogError("Broker", fmt.Sprintf("Configuration file does not exist: %s", configPath))
