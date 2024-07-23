f271127dbae0ff437064388561307e89b788cddd
JiajunZhou123 <jiajun.zhou1@ucdconnect.ie>
2024-07-23 01:40:41 +0100

    Add the functionality for exporting logs

--- web-app/src/components/Logs.jsx
+++ web-app/src/components/Logs.jsx
@@ -8,11 +8,20 @@ export default function Logs(props) {
 
     const { logsTitle, logsBackgroundColor, logsData } = props
 
+    const exportLogs = () => {
+        const file = new Blob([logsData.join('\n')], { type: 'text/plain' });
+        const url = URL.createObjectURL(file);
+        downloadLinkRef.current.href = url;
+        downloadLinkRef.current.download = `${logsTitle}.txt`;
+        downloadLinkRef.current.click();
+        URL.revokeObjectURL(url);
+    };
+
     return (
         <div>
             <div className='flex justify-between'>
                 <h2 className='mb-3 text-gray-500 font-medium'>{logsTitle}</h2>
-                <Button>Export Logs</Button>
+                <Button type='primary'>Export Logs</Button>
                 <a ref={downloadLinkRef} style={{ display: 'none' }} href='/'>Download</a>
             </div>
             <div className={`${logsBackgroundColor} py-5 px-5 mb-10 max-h-60 overflow-y-auto overflow-x-hidden`}>
