cb94f376967be0b370acbd39a0cfa899c7748b0a
JiajunZhou123 <jiajun.zhou1@ucdconnect.ie>
2024-07-23 01:44:38 +0100

    Bug fixed: use session to store data

--- web-app/src/store/index.js
+++ web-app/src/store/index.js
@@ -4,14 +4,14 @@ import producerReducer from './producerSlice';
 import consumerReducer from './consumerSlice';
 import brokerReducer from './brokerSlice';
 import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import storageSession from 'redux-persist/lib/storage/session';
 import { combineReducers } from 'redux';
 import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 
 const persistConfig = {
   key: 'root',
-  storage,
-  whitelist: ['user'],
+  storage: storageSession,
+  whitelist: ['user', 'producer', 'consumer', 'broker'],
 };
 
 const rootReducer = combineReducers({
