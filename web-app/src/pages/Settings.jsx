e5b407bb1168f5eff3b18c503ec2478fac06af93
JiajunZhou123 <jiajun.zhou1@ucdconnect.ie>
2024-07-23 01:50:49 +0100

    Add the warning number interface

--- web-app/src/pages/Settings.jsx
+++ web-app/src/pages/Settings.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { Button, Form, InputNumber } from 'antd';
 
 export default function Settings() {
+
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+  const onChange = (value) => {
+    console.log('changed', value);
+  };
+
   return (
-    <div>Settings</div>
+    <div>
+      <Form
+        name="settingsForm"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        style={{
+          maxWidth: 600,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="warning number"
+          name="warningNumber"
+        >
+          <InputNumber min={0} defaultValue={0} onChange={onChange} />;
+        </Form.Item>
+      </Form>
+    </div>
   )
 }
